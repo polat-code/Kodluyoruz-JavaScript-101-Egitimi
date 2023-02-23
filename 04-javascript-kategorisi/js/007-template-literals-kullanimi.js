750e1a53a46a1174c5931406aa9255ca3d20ba45
Özgürhan Polat <56520139+polat-code@users.noreply.github.com>
2023-02-23 23:07:44 +0300

    Templeare Literals Kullanim Örneği Yapıldı!

--- 04-javascript-kategorisi/js/007-template-literals-kullanimi.js
+++ 04-javascript-kategorisi/js/007-template-literals-kullanimi.js
@@ -1,22 +1,20 @@
 // ********** Template Literals Kullanimi ********** 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 
-let username = "hakan"
-const DOMAIN = "kodluyoruz.org"
+let userName = "ozgurhan";
+const DOMAIN = "kodluyoruz.org";
 
-let email = username + "@" + DOMAIN
 
-// console.log("Merhaba", username, "sitemize hosgeldin", "mail adresin:", email)
+let email = userName + "@" + DOMAIN;
 
-let info = `
-Merhaba ${username} sitemize hosgeldin.. 
-mail adresin: ${email}
+let info = `Merhaba ${userName} sitemize hoşgeldin..
+mail addresin ${email}
 
-mail adresinin uzunlugu: ${email.length}
-borcunuz: ${(2 + 3) * 10} TL
-gunun saat bilgisi : ${new Date().getHours()}
+kisa isminiz : ${userName[5]}
 
-kisa isminiz: ${username[0]}.
+Mail adresinin uzunluğu ${email.length}
+borcunuz: ${ (2 + 3) * 10} TL
+günün saat bilgisi ${new Date().getHours()}:${new Date().getMinutes()}
 `
+console.log(info);
 
-console.log(info)
