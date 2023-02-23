b7d8c752fa24282e002737095ef73244baed9809
Özgürhan Polat <56520139+polat-code@users.noreply.github.com>
2023-02-23 23:29:53 +0300

    Template Literals Kullanimi ile ilgili örnek yaptım

--- 04-javascript-kategorisi/js/007-template-literals-kullanimi.js
+++ 04-javascript-kategorisi/js/007-template-literals-kullanimi.js
@@ -18,3 +18,60 @@ günün saat bilgisi ${new Date().getHours()}:${new Date().getMinutes()}
 `
 console.log(info);
 
+
+// Example 
+//Convert this into Template Literals
+const kitap = {
+    ad: "Fırtına",
+    yazar: "Shakespeare",
+    tarih: 1610
+  }
+  const bookTable =
+        "<table border>"+
+    "<tbody>"+
+      "<tr>"+
+        "<td>"+"Kitap"+"</td>"+
+        "<td>"+kitap.ad+"</td>"+
+      "</tr>"+
+    "<tr>"+
+        "<td>"+"Yazar"+"</td>"+
+        "<td>"+kitap.yazar+"</td>"+
+      "</tr>"+
+        "<tr>"+
+        "<td>"+"Tarih"+"</td>"+
+        "<td>"+kitap.tarih+"</td>"+
+      "</tr>"+
+   " </tbody>"+
+  "</table>"
+ // document.body.innerHTML = bookTable
+
+const kitap01  = {
+    ad: "Fırtına",
+    yazar: "Shakespare",
+    tarih : 1610
+}
+
+const bookTable01 = `
+<table border>
+    <tbody>
+        <tr>
+        <td>Kitap</td>
+        <td>${kitap01.ad}</td>
+        </tr>
+        <tr>
+        <td>Yazar</td>
+        <td>${kitap01.yazar}</td>
+        </tr>
+        <tr>
+        <td>Tarih</td>
+        <td>${kitap01.tarih}</td>
+        </tr>
+
+    </tbody>
+</table>`
+;
+document.body.innerHTML = bookTable01;
+
+
+
+
